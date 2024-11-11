199dd92f8f09ec7b37320d372111eca8b7149449
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2024-11-11 22:58:54 +0530

    Print names n times

--- Recursion/Print Name N times/Names.java
+++ Recursion/Print Name N times/Names.java
@@ -1,5 +1,17 @@
+import java.util.*;
 class Names{
+
+  static void printName(int i,int n,String name){
+    if(i>n) return ;
+    System.out.println(name);
+    printName(i+1, n, name);
+  }
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    int n = sc.nextInt();
+    Names.printName(1,n,name);
+    sc.close();
   }
 }
