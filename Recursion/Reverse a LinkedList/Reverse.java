addf77d1cded0c34d85797d111603bfb6b04d4ca
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2024-11-11 23:48:46 +0530

    reverse final 1

--- Recursion/Reverse a LinkedList/Reverse.java
+++ Recursion/Reverse a LinkedList/Reverse.java
@@ -1,18 +1,20 @@
-//Only the Function is given here
+// Only the Function is given here
 
-/* 
-class Node{
-  int data;
-  Node next;
-}
-*/
-
-public static Node reverse(Node head){
-
-  if( head == null || head.next == null ) return head;
-  Node newNode = reverse(head.next);
-  Node front = head.next;
-  front.next = head;
-  head.next = null;
-  return newNode;
-}
+/*
+ * public static Node reverse(Node head){
+ * 
+ *  if( head == null || head.next == null ) return head;
+ *  
+ *  Node newNode = reverse(head.next);
+ *  
+ *  Node front = head.next;
+ *  
+ *  front.next = head;
+ *  
+ *  head.next = null;
+ *  
+ *  return newNode;
+ * }
+ * 
+ * 
+ */
